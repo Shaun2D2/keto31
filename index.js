61a335f436d9e3779ce945a4bd981bd26d1dc4a1
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-07-20 00:22:23 -0700

    added server templates

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('app is running'));
+app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('dist'));
+
+app.get('/', (req, res) => res.render('./templates/index'));
 
 app.listen(8080, () => console.log('app is running!'));
